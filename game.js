c3e9e5d967e38a374c12ca5c680d309cd83b6b4e
Orry Baram <orrybaram@gmail.com>
2013-05-14 20:00:42 -0400

    kill enemies forever

--- game.js
+++ game.js
@@ -2,10 +2,13 @@ var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
 var world;
-var keys = [];
 var enemies = [];
 var player;
+
 var SCALE = 30;
+var score = 0;
+
+var keys = [];
 
 var player_sprite = new Image();
     player_sprite.src = "images/player1.png";
@@ -89,21 +92,7 @@ function init() {
 
     player = world.CreateBody(bodyDef).CreateFixture(fixDef);
     
-
-    // CREATE ENEMIES
-    // ======================================================
-    for(var i = 0; i < 6; i++) {
-        bodyDef.type = b2Body.b2_dynamicBody;
-        bodyDef.position.x = Math.random() * 25;
-        bodyDef.position.y = Math.random() * 25
-        
-        fixDef.shape = new b2PolygonShape;
-        fixDef.shape.SetAsBox(10 / SCALE, 10 / SCALE)
-        fixDef.userData = 'enemy' + i;
-
-        enemy = world.CreateBody(bodyDef).CreateFixture(fixDef);
-        enemies.push(enemy)
-    }
+    createEnemies();
     
     setUpDebug();
 };
@@ -130,8 +119,31 @@ function update() {
     requestAnimFrame(update);
 
     destroyObjects();
+    updateScore();
+
+    checkStatus();
 
 };
+
+// CREATE ENEMIES
+    // ======================================================
+    function createEnemies() {
+        for(var i = 0; i < 6; i++) {
+            var bodyDef = new b2BodyDef;
+            var fixDef = new b2FixtureDef;
+
+            bodyDef.type = b2Body.b2_dynamicBody;
+            bodyDef.position.x = Math.random() * 25;
+            bodyDef.position.y = Math.random() * 25
+            
+            fixDef.shape = new b2PolygonShape;
+            fixDef.shape.SetAsBox(10 / SCALE, 10 / SCALE)
+            fixDef.userData = 'enemy' + i;
+
+            enemy = world.CreateBody(bodyDef).CreateFixture(fixDef);
+            enemies.push(enemy)
+        }
+    }
 
 function handleKeyDown(evt){
     keys[evt.keyCode] = true;
@@ -181,6 +193,8 @@ function detectCollisons() {
             console.log('hit enemy')
 
             trash.push(contact.m_fixtureB.m_body);
+            enemies.pop();
+            score +=1;
         }
         
     }
@@ -235,6 +249,25 @@ function destroyObjects() {
     for(var i = 0; i < trash.length; i++) {
         world.DestroyBody(trash[i]);
     }
+}
+
+function checkStatus() {
+    if (enemies.length === 0) {
+        showWinScreen();
+    }
+}
+
+function updateScore() {
+    $('#score').html(score);
+}
+
+function showWinScreen() {
+    console.log('YOu WON')
+    resetGame()
+}
+
+function resetGame() {
+    createEnemies();
 }
 
 function setUpDebug() {
